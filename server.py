f75110f8b0c86da5fd2dfed097c9c2ec2a838a4e
Vinay <vinayvilasmore@gmail.com>
2025-04-08 14:23:29 +0530

    cors fixes

--- server.py
+++ server.py
@@ -21,7 +21,7 @@ nltk.download('stopwords')
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "http://localhost:5173"]}})
+CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "http://localhost:5173","https://news-app-eight-liart.vercel.app"]}})
 
 MODEL_PATH = './model/svm_model.pkl'
 VECTORIZER_PATH = './model/vectorizer.pkl'
